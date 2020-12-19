cf50e00b570ae3c66827ebab7be280f7cce95e85
Dipalee30 <dipaleepawar30@gmail.com>
2020-12-18 22:12:44 -0500

    Final Code

--- mmp/src/main/java/iit/healthcare/utility/AppLibrary.java
+++ mmp/src/main/java/iit/healthcare/utility/AppLibrary.java
@@ -1,12 +1,18 @@
 package iit.healthcare.utility;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.testng.annotations.DataProvider;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -15,6 +21,8 @@ import jxl.read.biff.BiffException;
 
 public class AppLibrary {
 
+	
+	
 	public static String selectFutureDate(int days)
 	{
 		Calendar cal= Calendar.getInstance();
@@ -27,45 +35,64 @@ public class AppLibrary {
 		return formattedDate; 
 	}
 	
-	@DataProvider(name="inputdata")
-	public static String[][] readXLS(String fileName ) throws BiffException, IOException
+
+	public static String[][] readXLS(String filePath ) throws BiffException, IOException
 	{
-		File f=new File("F:\\QA Training\\RADIUS -Selenium Healthcare Project\\Login.xls");
-		/*fetch the excelfrom above provided location and store it an object*/
-	
+		File f=new File(filePath);
 		Workbook w= Workbook.getWorkbook(f);		 
-		Sheet s= w.getSheet("Sheet1");	 
+		Sheet s= w.getSheet(0);	 
 		//number of rows in the sheet having data		 
 		int rows= s.getRows();
 		//number of columns in the sheet having data
 		int col= s.getColumns();
-		// go to each row.its each column and fetch the datafrom cell
-		//Create an array which is going to store all the values of rows and columns
+		 /*go to each row.its each column and fetch the datafrom cell
+		Create an array which is going to store all the values of rows and columns */
 
-		String inputdata[][]=new String[rows][col];
+		String inputdata[][]=new String[rows-1][col];
 
 		//read the data
-		for(int i=0;i<rows;i++)
+		for(int i=1;i<rows;i++)
 		{
 			for(int j=0;j<col;j++) 
 			{
 				//fetch the data from each cell of that row
-				Cell c= s.getCell(j,i);
+				Cell cell= s.getCell(j,i);
 				//once you have the cell value fetch the contents
-				inputdata[i][j]=c.getContents();
+				inputdata[i-1][j]=cell.getContents().toString();
 
 			}
 		}
 
 		return inputdata;
 	}
-	 
-	 
-	 public static String[][] readXLSX(String fileName)
-	 {
-	  return new String[10][10];
-	 }
+
 	
 	
+     public static String[][] readXlsx(String filePath) throws IOException{
+		
+		File srcFile = new File(filePath);
+		FileInputStream fis = new FileInputStream(srcFile);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int row = sheet.getPhysicalNumberOfRows();
+		int col = sheet.getRow(0).getPhysicalNumberOfCells();
+		String[][] data = new String[row][col];
+		 XSSFCell cell;
+		for (int i=0; i<row; i++)
+		{
+			for(int j=0;j<col;j++)
+			{
+				cell=sheet.getRow(i).getCell(j);
+				String cellContents= cell.getStringCellValue();
+				data[i][j]= cellContents;
+				
+			}
+		}
+		
+		return data;		
+		
+	}	
 	
-}
+}	
+	
+
--- mmp/src/test/java/org/iit/healthcare/mmp/patientmodule/tests/LoginTest.java
+++ mmp/src/test/java/org/iit/healthcare/mmp/patientmodule/tests/LoginTest.java
@@ -1,5 +1,7 @@
 package org.iit.healthcare.mmp.patientmodule.tests;
 
+import java.io.IOException;
+
 import org.iit.healthcare.base.TestBaseClass;
 import org.iit.healthcare.mmp.patientmodule.pages.LoginPage;
 import org.testng.annotations.DataProvider;
@@ -7,8 +9,12 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import iit.healthcare.utility.AppLibrary;
+import jxl.read.biff.BiffException;
 
 public class LoginTest extends TestBaseClass {
+	
+ 
+	
 	/*@Parameters({"uname","pword"})
 	@Test
 	public void login(String uname,String pword)
@@ -25,7 +31,7 @@ public class LoginTest extends TestBaseClass {
 	}*/
 	
 	
-	@Test(dataProvider="inputdata")
+	@Test(dataProvider="testData")
 	public void login(String uname,String pword)
 	{
 		launchApplication("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
@@ -38,11 +44,22 @@ public class LoginTest extends TestBaseClass {
 	}
 	
 	
+	/*@DataProvider (name="testData")
+	public String[][] loginData() throws IOException, BiffException
+	{
+		 String[][] loginData=AppLibrary.readXLS("Login1.xls");
+		 return loginData;
+		
+		
+	}*/
 	
-	
-	
-	
-	
+	@DataProvider (name="testData")
+	 public String[][] loginData2() throws IOException
+	 {
+		 String[][] loginData2=AppLibrary.readXlsx("Login2.xlsx");
+		 
+		 return loginData2;
+	 }
 	
 	
 	
